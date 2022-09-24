257e2c2f929ac0d20680e59e29322dd534f59159
Harry-KNIGHT <eliott.knight@icloud.com>
2022-09-24 09:57:55 +0200

    Httpurl réponse condition modified

--- Sources/Crypto4FunKit/Api/FetchChartApi.swift
+++ Sources/Crypto4FunKit/Api/FetchChartApi.swift
@@ -26,7 +26,7 @@ public struct ChartApi {
 
 		let (data, response) = try await URLSession.shared.data(from: url)
 
-		guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+		guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode >= 200 && httpResponse.statusCode < 300 else {
 			print(response)
 			throw ApiError.outOfBounds
 		}
