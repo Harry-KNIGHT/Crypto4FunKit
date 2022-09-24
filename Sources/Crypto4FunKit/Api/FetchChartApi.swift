e5aa7959de1e293b5696941227025f5d877a18fc
Harry-KNIGHT <eliott.knight@icloud.com>
2022-09-24 09:48:41 +0200

    Print response status if chart request fail.

--- Sources/Crypto4FunKit/Api/FetchChartApi.swift
+++ Sources/Crypto4FunKit/Api/FetchChartApi.swift
@@ -27,6 +27,7 @@ public struct ChartApi {
 		let (data, response) = try await URLSession.shared.data(from: url)
 
 		guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+			print(response)
 			throw ApiError.outOfBounds
 		}
 
